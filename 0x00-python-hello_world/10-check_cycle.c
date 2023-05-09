784a3b0f2b58413d6d4b8829b2acc94cd2344a60
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-09 02:40:35 +0100

    Write Python

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,14 @@
+#include "list.h"
+
+/**
+ * check_cycle -> Checks if a singly linked list has a cycle in it
+ * 
+ * @list: Linked List
+ * 
+ * Return: Depend Condition
+*/
+
+int check_cycle(listint_t *list)
+{
+    
+}
