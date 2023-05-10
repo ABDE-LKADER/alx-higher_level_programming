f96ee2c4429e2d33afe62cf453b1a5955252b050
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-10 01:27:22 +0100

    00 TO 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for x in range(0, 100):
-    if x < 10:
-        print("0{:d}".format(x), end='')
-    else:
-        print("{:d}".format(x), end='')
+    print("{:02d}".format(x), end='')
     if x < 99:
         print(", ", end='')
