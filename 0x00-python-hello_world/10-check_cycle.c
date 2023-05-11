a240cf2be25d63fd88925fab9e1c72418df0da43
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-11 03:20:09 +0100

    Solving Problem

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * check_cycle -> Check if Has cycle or not
+ *
+ * @list: Linked List
+ *
+ * Return: Depend Condition
+*/
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow, *fast;
+
+	if (list == NULL)
+		return (0);
+	slow = list;
+	fast = list->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+
+	return (0);
+}
