864826be4a894cb139369b40d6593802598c4546
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-12 00:58:14 +0100

    NOT WORK

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/Python3.8.x
 if __name__ == "__main__":
     import hidden_4
-    for name in dir(hidden_4):
+    for name in sorted(dir(hidden_4)):
         if not name.startswith('__'):
             print(name)
