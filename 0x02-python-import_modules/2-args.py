c7cccc9086dbf7d56d2b9462c3dafc0170c8e7c1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-11 22:54:13 +0100

    Dynamic Script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,9 +3,10 @@ if __name__ == "__main__":
     import sys
     if len(sys.argv) - 1 == 0:
         print("0 arguments.")
-    elif len(sys.argv) - 1 == 1:
-        print("1 argument:")
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        if len(sys.argv) - 1 == 1:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
