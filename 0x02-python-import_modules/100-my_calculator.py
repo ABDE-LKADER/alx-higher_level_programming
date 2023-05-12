dc45c7c62c8759f6d096a5614684712a090f2651
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-12 01:52:12 +0100

    Own Calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,22 +4,21 @@ if __name__ == "__main__":
     from calculator_1 import *
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit ("1")
+        sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     operator = sys.argv[2]
-    if operator == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-        exit ("0")
-    elif operator == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-        exit ("0")
-    elif operator == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-        exit ("0")
-    elif operator == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
-        exit ("0")
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
-        exit ("1")
+    result = 0
+    match operator:
+        case '+':
+            result = add(a, b)
+        case '-':
+            result = sub(a, b)
+        case '*':
+            result = mul(a, b)
+        case '/':
+            result = div(a, b)
+        case _:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+    print("{} {} {} = {}".format(a, operator, b, result))
