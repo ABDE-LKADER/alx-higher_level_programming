af48ea8b8d18ae0fc6255ba188f2489223b2e44f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-16 03:18:17 +0100

    MAX

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,5 +6,5 @@ def max_integer(my_list=[]):
             if my_list[i] > num_max:
                 num_max = my_list[i]
     else:
-        return ("None")
+        return "None"
     return num_max
