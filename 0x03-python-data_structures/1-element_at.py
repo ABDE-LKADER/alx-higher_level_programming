b7759751a60650a8cabe01c16b0ab88af062c3b0
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-16 01:44:01 +0100

    Retrieves Elm

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,5 +3,5 @@ def element_at(my_list, idx):
     if idx < 0:
         return ("None")
     if idx > len(my_list) - 1:
-            return ("None")
+        return ("None")
     return(my_list[idx])
