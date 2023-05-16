6321d576643312386957187855f8c39a1f05e56b
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-16 02:21:20 +0100

    Remove Char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    new_str = ""
-    for i in my_string:
-        if i != "c" and i != "C":
-            new_str += i
-    return new_str
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+    return new_string
