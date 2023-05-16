9472ca338dcd0195a6242607fd44df30689d6609
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-16 02:34:54 +0100

    Remove Char

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    str_translate  = {ord(i): None for i in 'cC'}
-    new_string = my_string.translate(str_translate)
+    new_string = my_string.translate({ord(i): None for i in 'cC'})
     return (new_string)
