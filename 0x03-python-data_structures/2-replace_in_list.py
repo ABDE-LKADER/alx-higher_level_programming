ffc161e54b41d24b89b53f078eac5e5efce2171c
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-16 01:50:13 +0100

    List Change

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -4,5 +4,5 @@ def replace_in_list(my_list, idx, element):
         return (my_list)
     if idx > len(my_list) - 1:
         return (my_list)
-    my_list[idx]=element
+    my_list[idx] = element
     return (my_list)
