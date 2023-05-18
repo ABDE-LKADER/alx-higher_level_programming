cca4cee034e9fba2e5e8fcdb8e6babc01966e4ce
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:45:01 +0100

    CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,34 +2,6 @@
 #include <stdio.h>
 
 /**
- * print_python_list -> prints some basic info about Python lists
- *
- * @p: Python Object
-*/
-
-void print_python_list(PyObject *p)
-{
-	long int size, i;
-	PyListObject *list;
-	PyObject *obj;
-
-	size = ((PyVarObject *)(p))->ob_size;
-	list = (PyListObject *)p;
-
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-	}
-}
-
-/**
  * print_python_bytes -> prints some basic info about Python bytes
  *
  * @p: Python Object
@@ -67,4 +39,32 @@ void print_python_bytes(PyObject *p)
 			printf(" %02x", 256 + string[i]);
 
 	printf("\n");
+}
+
+/**
+ * print_python_list -> prints some basic info about Python lists
+ *
+ * @p: Python Object
+*/
+
+void print_python_list(PyObject *p)
+{
+	long int size, i;
+	PyListObject *list;
+	PyObject *obj;
+
+	size = ((PyVarObject *)(p))->ob_size;
+	list = (PyListObject *)p;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", list->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		obj = ((PyListObject *)p)->ob_item[i];
+		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
+		if (PyBytes_Check(obj))
+			print_python_bytes(obj);
+	}
 }
