d9ed58d00506e07a1f5adfbb84efdb935be3e822
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:08:41 +0100

    MULT By 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     new_dictionary = a_dictionary.copy()
-    for i in list(a_dictionary.keys()):
+    for i in list(new_dictionary.keys()):
         new_dictionary[i] *= 2
     return  new_dictionary
