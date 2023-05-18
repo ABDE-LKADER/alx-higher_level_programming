f67ffd3100e309a58f8f306bad46ac7dccc92609
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:20:23 +0100

    ALL VAL

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    all_val = list(map((lambda i: i * number), my_list))
-    return (all_val)
+    return (list(map((lambda i: i * number), my_list)))
