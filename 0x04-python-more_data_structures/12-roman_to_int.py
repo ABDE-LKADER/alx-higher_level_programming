f0f5698884be14cc07446e3df99b8e343d6f2ec0
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:26:25 +0100

    Roman TC Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if type(roman_string) != str or not roman_string:
+    if not roman_string or type(roman_string) != str:
         return 0
     sum_all = 0
     digits_roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
