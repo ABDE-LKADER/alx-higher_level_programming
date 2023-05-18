cdc268c1b330c0a68d14bbd6e152580abc0266d1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:36:55 +0100

    DELETE Value

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for i in list(a_dictionary.keys()):
         if value == a_dictionary.get(i):
             del a_dictionary[i]
-    return (a_dictionar)
+    return (a_dictionary)
