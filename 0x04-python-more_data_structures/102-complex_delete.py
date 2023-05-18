8ae88c7f027987ddf7f3422f438812be0471c781
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:36:33 +0100

    Squared MAP

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for i in list(a_dictionary.keys()):
         if value == a_dictionary.get(i):
             del a_dictionary[i]
-    return (a_dictionary)
+    return (a_dictionar)
