e222f8fbe6d4b57070c7449515c85db0faed8149
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:16:36 +0100

    Max VAL

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if not a_dictionary:
         return None
     best_sc = max(a_dictionary, key=a_dictionary.get)
     return best_sc
