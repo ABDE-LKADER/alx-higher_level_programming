1f0242f8b2df72aa22e2ab2091f90a62a962f944
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-18 03:28:49 +0100

    Roman TC Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    if type(roman_string) != str or not roman_string:
         return 0
     sum_all = 0
-    digits_roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for r in reversed(roman_string):
-        digits_10 = digits_roman[r]
-        sum_all += digits_10 if sum_all < digits_10 * 5 else -digits_10
+    roman = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    for roman in reversed(roman_string):
+        digits = roman[roman]
+        sum_all += digits if sum_all < digits * 5 else -digits
     return sum_all
