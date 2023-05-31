efa9588af6fde02095279a730b109d0d1e0d268f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-31 03:55:49 +0100

    0x06. Python -> Classes and Objects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,10 +5,10 @@ class Square:
     """Represents a square"""
     def __init__(self, size=0):
         """Initializes the data"""
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >=0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
     self.__size = size
 
     def area(self):
--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -5,3 +5,24 @@ class Square:
     """Represents a square"""
     def __init__(self, size=0):
         """Initializes the data"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
+
+    def size(self):
+        """Retrieves the size"""
+        return self.__size
+    
+    def size(self, value):
+        """Sets the size to a value"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+    
+    def area(self):
+        """Return the current square area"""
+        return (self.__size * self.__size)
