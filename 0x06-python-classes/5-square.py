b4841b64a1e05147e5647cdff44127a0004d593a
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-01 03:45:42 +0100

    Python -> Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -8,10 +8,6 @@ class Square:
     def __init__(self, size=0):
         """Initializes the data"""
 
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >=0")
         self.size = size
 
     @property
@@ -19,10 +15,10 @@ class Square:
         """Retrieve size"""
 
         return self.__size
-    
+
     @size.setter
     def size(self, value):
-        """Sets size"""
+        """Sets Size"""
 
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
@@ -36,7 +32,7 @@ class Square:
         return (self.__size * self.__size)
     
     def my_print(self):
-        """Prints in stdout the square with the character #"""
+        """print the square in # """
 
         if self.__size == 0:
             print()
