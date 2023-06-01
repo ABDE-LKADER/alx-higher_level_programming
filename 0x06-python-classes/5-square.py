3eee40d15d3b6d751d6ca8ca1d23260fb53150c0
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-01 03:42:22 +0100

    Python -> Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""Defines a square bassed on 4-square.py"""
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size=0):
+        """Initializes the data"""
+
+        self.size = size
+
+    @property
+    def size(self):
+        """Retrieve size"""
+
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        """Sets size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def area(self):
+        """Return the current square area"""
+
+        return (self.__size * self.__size)
+    
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+
+        if self.__size == 0:
+            print()
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print()
