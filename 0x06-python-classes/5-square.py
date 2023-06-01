266895a96b1393a7ffebe9ae6f1d2ce01b537a2d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-01 03:43:51 +0100

    Python -> Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -37,6 +37,4 @@ class Square:
         if self.__size == 0:
             print()
         for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
-            print()
+            print("#" * self.__size)
