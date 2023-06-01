ff45be1fb1041913e9aea838f447a0bbf247da2e
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-01 03:44:40 +0100

    Python -> Classes and Objects

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -8,6 +8,10 @@ class Square:
     def __init__(self, size=0):
         """Initializes the data"""
 
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >=0")
         self.size = size
 
     @property
