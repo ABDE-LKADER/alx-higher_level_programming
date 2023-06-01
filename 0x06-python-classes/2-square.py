234547c8964cee6d27ce788935823863d03ac8ab
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-01 03:12:44 +0100

    Python -> Classes and Objects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Defines a square bassed on 1-square.py"""
 
+
 class Square:
     """Represents a square"""
     def __init__(self, size=0):
         """Initializes the data"""
-        if not isinstance(size, int):
+        if size not digit:
             raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >=0")
+        else:
+            raise ValueError("size must be >= 0")
         self.__size = size
-
