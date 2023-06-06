0dadeac7dde1cbc3f47bff3c0b41b9c01b86609b
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-06 02:40:57 +0100

    Python -> More Classes and Objects

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+"""Defines a rectangle"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a rectangle"""
+
+        self.width = width
+        self.height = height
