b0904a1aab628ca98958e36b4d7df838b1dc2944
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-07 03:40:31 +0100

    Python -> Everything is object

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module creates a class named LockedClass"""
+
+
+class LockedClass:
+    """This class has no class or object attribute"""
+
+    __slots__ = ['first_name']
