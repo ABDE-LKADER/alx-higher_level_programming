3217790b1f2c31bc53d557aadb196c8913ea00b0
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-08 02:32:44 +0100

    Python -> Classes and Objects

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -9,8 +9,7 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >=0")
-    if isinstance(size, float) and size < 0:
-        raise TypeError("size must be an integer")
+
     for i in range(size):
         for j in range(size):
             print("#", end="")
