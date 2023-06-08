2aef3b708bda83898487a4a8f424223553c791c4
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-08 02:27:58 +0100

    Python -> Classes and Objects

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,9 +5,9 @@
 def say_my_name(first_name, last_name=""):
     """Prints My name is <first name> <last name>"""
 
-    if type(first_name) is not str:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if type(last_name) is not str:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {} {}".format(first_name, last_name))
