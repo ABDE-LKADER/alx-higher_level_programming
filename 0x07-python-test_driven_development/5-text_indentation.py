d11ed401c27570b58a046c35162143f3649fa737
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-08 03:15:42 +0100

    Python -> Classes and Objects

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,9 +8,11 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
 
-    for i in range(len(text)):
-        if text[i] == "." or text[i] == "?" or text[i] == ":":
-            print(text[i])
-            print()
-        else:
-            print(text[i], end="")
+    for i in ".?:":
+        list = text.split(i)
+        text = ""
+        for j in list:
+            text += j.strip(" ")
+            if j is not list[-1]:
+                text += i + "\n\n"
+    print(text, end="")
