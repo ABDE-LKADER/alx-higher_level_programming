e101b78d3e6e5a8e1959ea39222ce9ee7d05eb07
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 03:12:16 +0100

    Python -> Input/Output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""This module defines a JSON-to-object function"""
-
-
+"""Module that returns the Python object representation of a JSON string"""
 import json
 
 
 def from_json_string(my_str):
-    """Returns the Python object representation of a JSON string"""
+    """Function that returns the Python object representation of a JSON string"""
+
     return json.loads(my_str)
