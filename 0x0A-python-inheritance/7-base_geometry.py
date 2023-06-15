feb48353219ced6495c93422581074501f53c63e
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 02:51:03 +0100

    0x0A. Python -> Inheritance

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -7,13 +7,11 @@ class BaseGeometry:
 
     def area(self):
         """method not implemented yet"""
-
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-
-        """validates a value as an integer"""
-
+        """validates a value as an integer
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,8 +7,8 @@ class Rectangle(BaseGeometry):
     """a class to define rectangle using BaseGeometry"""
 
     def __init__(self, width, height):
-        """Intialize a new Rectangle
-        """
+        """Intialize a new Rectangle"""
+
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
