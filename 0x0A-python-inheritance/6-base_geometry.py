0bb73ee33b8b36dec8d6c722adf0435d21893176
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 02:49:36 +0100

    0x0A. Python -> Inheritance

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a base geometry class BaseGeometry."""
+"""Defines a base geometry class BaseGeometry"""
 
 
 class BaseGeometry:
@@ -7,4 +7,5 @@ class BaseGeometry:
 
     def area(self):
         """method not implemented yet"""
+
         raise Exception("area() is not implemented")
