623ecb67a69f0995eb7868b6e9aaa27085be3313
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 03:09:02 +0100

    Python -> Input/Output

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a string-to-JSON function"""
-import json
-
-
-def to_json_string(my_obj):
-    """Returns the JSON representation of a string object"""
-    return json.dumps(my_obj)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a JSON file-reading function"""
-import json
-
-
-def load_from_json_file(filename):
-    """Creates a Python object from a given JSON file"""
-    with open(filename) as f:
-        return json.load(f)
