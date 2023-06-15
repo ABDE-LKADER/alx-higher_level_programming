9d17fd845ed2b4672d8aeb761e9a36a81cc398f5
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 02:46:56 +0100

    0x0A. Python -> Inheritance

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,4 +4,5 @@
 
 def lookup(obj):
     """This functions looks out for all attributes and methods of an object"""
+
     return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,8 @@
 
 class MyList(list):
     """A class that inherits from list"""
+
     def print_sorted(self):
         """prints a sorted list"""
+
         print(sorted(self))
