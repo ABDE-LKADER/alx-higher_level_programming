cd6d3b4aebdda995b1cad31dc7b92b38f1309452
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 03:14:33 +0100

    Python -> Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
+"""Module adds all arguments to a Python list, and then save them to a file"""
 
 
 import sys
