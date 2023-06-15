39a43f3db82c9db30cab7b84864f3b685c388176
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 03:18:19 +0100

    Python -> Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,20 +3,18 @@
 
 
 class Student:
-    """Represent a student."""
+    """This class defines a Student object"""
 
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student
-        """
+        """Initializes an instance of a Student object"""
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Get a dictionary representation of the Student
-        If attrs is a list of strings, represents only those attributes
-        included in the list
-        """
+        """Returns the dictionary representation of a Student instance"""
+
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
