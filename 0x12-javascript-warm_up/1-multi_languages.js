a4044c3bf4a29bbc3012f0d97e780819a4669ddc
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-08-15 03:34:56 +0100

    JavaScript is amazing

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,3 +1,9 @@
 #!/usr/bin/node
-const myVar = 'JavaScript is amazing';
-console.log(myVar);
+
+const myVar = "C is fun"
+const myVar1 = "Python is cool"
+const myVar2 = "JavaScript is amazing"
+
+console.log(myVar)
+console.log(myVar1)
+console.log(myVar2)
