3b179355d84660da030ddae9618801febcb1d2d3
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-08-15 03:38:55 +0100

    JavaScript is amazing

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,3 @@
 #!/usr/bin/node
-const a = "No argument"
-const b = "Argument found"
-const c = process.argv.length;
-
-console.log(count === 2 ? a : count === 3 ? b : b);
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+let i;
+const myString = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (i = 0; i < 3; i++) {
+  console.log(myString[i]);
+}
