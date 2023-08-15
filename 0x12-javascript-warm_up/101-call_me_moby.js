9ef2ccb630aa50e7fe7112e72f844968138b4c98
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-08-15 03:40:53 +0100

    JavaScript is amazing

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+exports.callMeMoby = function (x, theFunction) {
+    for (let i = 0; i < x; i++) theFunction();
+  };
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,4 +3,3 @@ function add (a, b) {
     return a + b;
   }
   console.log(add(Number(process.argv[2]), Number(process.argv[3])));
-  
