9f52945d8e1248e75c4672f75eb16ce002d99402
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-08-17 01:24:56 +0100

    JavaScript - Warm up

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 function add (a, b) {
-    return a + b;
-  }
-  console.log(add(Number(process.argv[2]), Number(process.argv[3])));
+  return a + b;
+}
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
--- 0x12-javascript-warm_up/README.md
+++ 0x12-javascript-warm_up/README.md
@@ -1,1 +1,1 @@
-> New Start With __*JavaScript *__ Let's Go
+> New Start With __*JavaScript - Objects, Scopes and Closures*__ Let's Go
