278730bdbb86c59723c461dc65de5dea50d8e73f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-11-14 01:09:16 +0100

    JavaScript - Warm Up

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-const count = process.argv.length;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
