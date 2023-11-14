15246f33dd3390bff59c252e51ce7a6ebe0925d9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-11-14 22:53:12 +0100

    Javascript - Objects - Scopes - Closures

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -6,10 +6,5 @@ exports.nbOccurences = function (list, searchElement) {
       count++;
     }
   return (count);
-  });
+  }, 0);
 };
-const nbOccurences = require('./7-occurrences').nbOccurences;
-
-console.log(nbOccurences([1, 2, 3, 4, 5, 6], 3));
-console.log(nbOccurences([3, 2, 3, 4, 5, 3, 3], 3));
-console.log(nbOccurences(["S", 12, "c", "S", "School", 8], "S"));
