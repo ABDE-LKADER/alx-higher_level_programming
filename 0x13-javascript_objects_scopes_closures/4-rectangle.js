79d978117e68002780c9d853f025d1dd7802e1f0
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-11-14 17:23:51 +0100

    Javascript - Objects - Scopes - Closures

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -14,10 +14,12 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    [this.width, this.height] = [this.height, this.width];
+    this.width = this.height;
+    this.height = this.width;
   }
 
   double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
 };
