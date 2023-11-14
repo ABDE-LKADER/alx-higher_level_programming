c35088110631ea29068033bddc162c736928655c
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-11-14 17:29:06 +0100

    Javascript - Objects - Scopes - Closures

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
 module.exports = class Square extends require('./4-rectangle.js') {
   constructor (size) {
-    super (size, size);
+    super(size, size);
   }
 };
-
-const Square = require('./5-square');
-
-const s1 = new Square(4);
-s1.print();
-s1.double();
-s1.print();
